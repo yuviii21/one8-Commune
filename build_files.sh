c0e51d923d7679821c3166ea7d7258b2d59ad6bf
yuvi <g.yuvaraj2109@gmail.com>
2026-05-06 21:45:42 +0530

    Update deployment config and include generated static build artifacts.
    Adjust Vercel static routing/storage settings and commit the current full working tree state for deployment consistency.

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-python -m venv .venv
-source .venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput
--- meal_buddy/settings.py
+++ meal_buddy/settings.py
@@ -148,9 +148,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'delivery' / 'static']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
