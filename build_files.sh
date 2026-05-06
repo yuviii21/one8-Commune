758cd4feb267c30df3afddff65f5bd7cf0e5edef
yuvi <g.yuvaraj2109@gmail.com>
2026-05-06 21:09:44 +0530

    Use virtualenv during Vercel build to avoid managed-environment pip failures.
    Create and activate a local venv in the build script before installing requirements so deploys are compatible with stricter Python environments.

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-python -m pip install --break-system-packages -r requirements.txt
+set -e
+
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
