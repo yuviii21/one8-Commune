645e43b02ada029d8dd832cca7f7f2175faef071
yuvi <g.yuvaraj2109@gmail.com>
2026-05-07 10:23:16 +0530

    Auto-populate restaurants in Vercel build phase

--- build_files.sh
+++ build_files.sh
@@ -5,3 +5,4 @@ python -m pip install --upgrade pip --break-system-packages
 python -m pip install -r requirements.txt --break-system-packages
 python manage.py collectstatic --noinput
 python manage.py migrate
+python populate_restaurants.py
