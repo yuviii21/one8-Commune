ba622330fa5f1b2f2f13f7ffb61598f2fe9f89fb
yuvi <g.yuvaraj2109@gmail.com>
2026-05-07 09:42:38 +0530

    Fix externally-managed-environment pip error on Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python -m pip install --upgrade pip --break-system-packages
+python -m pip install -r requirements.txt --break-system-packages
 python manage.py collectstatic --noinput
 python manage.py migrate
