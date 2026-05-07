a419a07082c03d41224a7a073e138d140479a6fb
yuvi <g.yuvaraj2109@gmail.com>
2026-05-07 10:05:37 +0530

    Increase model field max_lengths to fix PostgreSQL too long errors

--- delivery/models.py
+++ delivery/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    username = models.CharField(max_length = 20)
-    password = models.CharField(max_length = 20)
-    email = models.CharField(max_length = 20)
-    mobile = models.CharField(max_length = 10)
-    address = models.CharField(max_length = 50)
+    username = models.CharField(max_length = 50)
+    password = models.CharField(max_length = 128)
+    email = models.CharField(max_length = 254)
+    mobile = models.CharField(max_length = 15)
+    address = models.CharField(max_length = 255)
 
 class AdminUser(models.Model):
-    username = models.CharField(max_length = 20, unique=True)
-    password = models.CharField(max_length = 20)
-    email = models.CharField(max_length = 20)
+    username = models.CharField(max_length = 50, unique=True)
+    password = models.CharField(max_length = 128)
+    email = models.CharField(max_length = 254)
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length = 20)
-    picture = models.URLField(max_length = 200, default='https://designshack.net/wp-content/uploads/Free-Simple-Restaurant-Logo-Template.jpg')
+    name = models.CharField(max_length = 100)
+    picture = models.URLField(max_length = 400, default='https://designshack.net/wp-content/uploads/Free-Simple-Restaurant-Logo-Template.jpg')
     cuisine = models.CharField(max_length = 200)
     rating = models.FloatField()
     
 class Item(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE, related_name = "items")
-    name = models.CharField(max_length = 20)
-    description = models.CharField(max_length = 200)
+    name = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 500)
     price = models.FloatField()
     vegeterian = models.BooleanField(default=False)
     picture = models.URLField(max_length = 400, default='https://www.indiafilings.com/learn/wp-content/uploads/2024/08/How-to-Start-Food-Business.jpg')
